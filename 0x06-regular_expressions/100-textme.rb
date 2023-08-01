6272dfc65bc4dec31e93b634bdd68cd24f43605c
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-08-01 15:44:12 -0700

    solve task no 9

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 sender = ARGV[0].scan(/from:(.*?)\]/)
 reciever = ARGV[0].scan(/to:(.*?)\]/)
 flags = ARGV[0].scan(/flags:(.*?)\]/)
-puts [sender, reciever, flags].join
+puts [sender, reciever, flags].join(',')
