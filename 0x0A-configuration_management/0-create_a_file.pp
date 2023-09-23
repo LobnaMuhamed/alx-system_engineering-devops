543edb97d17962baf9491bf21aae8880b533b7b6
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-22 18:21:11 -0700

    fix task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Create File
 
 file { '/tmp/school':
-  ensure    => file,
-  mode      => '0744',
-  owner     => 'www-data',
-  group     => 'www-data',
-  content   => 'I love Puppet',
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
