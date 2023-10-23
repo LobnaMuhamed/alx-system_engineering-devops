857bd17c8693be31f873230726d77bdd8e77856c
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-10-23 12:53:09 -0700

    test file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-"""
-Write a Python script that,
-using this REST API, for a given employee ID,
-returns information about
-his/her TODO list progress.
+""" Write a Python script that, using this REST API,
+    for a given employee ID,
+    returns information about his/her TODO list progress.
 """
 
 
 if __name__ == "__main__":
     import requests
     from sys import argv
+
     if (len(argv) == 2):
         id = int(argv[1])
-        url_user = f"https://jsonplaceholder.typicode.com/users/{id}"
-        user = requests.get(url_user).json()
-        url_todo = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
-        todo = requests.get(url_todo).json()
-        total_tasks = len(todo)
-        done_filter = list(filter(lambda obj: obj['completed'], todo))
-        len_done_filter = len(done_filter)
-        print(f"Employee {user['name']} is done with \
-tasks({len_done_filter}/{total_tasks}):")
+        user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+        user = requests.get(user_url).json()
+        todo_url = f"https://jsonplaceholder.typicode.com/todos/"
+        req_todo = requests.get(todo_url).json()
+        todo_filter = list(filter(lambda obj: obj['userId'] == id, req_todo))
+        done_filter = list(filter(lambda obj: obj['completed'], todo_filter))
+
+        user_name = user['name']
+        total_tasks = len(todo_filter)
+        done_tasks = len(done_filter)
+
+        print(f"Employee {user_name} is done with \
+tasks({done_tasks}/{total_tasks}):")
         for task in done_filter:
             print("\t ", end='')
             print(task['title'])
