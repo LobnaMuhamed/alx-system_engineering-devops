f4c3cf46f1f6c411cdf7a332c8de10b34281284e
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-10-23 13:09:58 -0700

    solve task no 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Write a Python script that,
+using this REST API, for a given employee ID,
+returns information about
+his/her TODO list progress.
+"""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    users_url = f"https://jsonplaceholder.typicode.com/users/"
+    users = requests.get(users_url).json()
+    todos_url = f"https://jsonplaceholder.typicode.com/todos/"
+    todos = requests.get(todos_url).json()
+
+    file_name = f"todo_all_employees.json"
+    tasks = {}
+
+    for user in users:
+        tasks[user['id']] = []
+
+        for task in todos:
+            if (task['userId'] == user['id']):
+                obj = {
+                        "username": user['username'],
+                        "task": task['title'],
+                        "completed": task['completed'],
+                }
+                tasks[user['id']].append(obj)
+
+    with open(file_name, 'w') as file:
+        json.dump(tasks, file)
